87814d5a56f8ed45b49f8adf7d86f1a15a0e2ed2
Adnan Hemani <adnan.h@berkeley.edu>
2016-10-20 08:28:34 +0000

    SimpleCov still tryin

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,11 @@
 require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/config/'
-  add_filter '/lib/'
-  add_filter '/vendor/'
-  add_filter '/features/'
-  #add_filter '/coverage/'
- 
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Views', 'app/views'
+SimpleCov.start 'rails' do
+  # add_filter '/spec/'
+  # add_filter '/config/'
+  # add_filter '/lib/'
+  # add_filter '/vendor/'
+  # add_filter '/features/'
+  # #add_filter '/coverage/'
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
--- spec/support/env.rb
+++ spec/support/env.rb
@@ -1,16 +1,10 @@
 require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/config/'
-  add_filter '/lib/'
-  add_filter '/vendor/'
-  add_filter '/features/'
-  #add_filter '/coverage/'
+SimpleCov.start 'rails' do
+  # add_filter '/spec/'
+  # add_filter '/config/'
+  # add_filter '/lib/'
+  # add_filter '/vendor/'
+  # add_filter '/features/'
+  # #add_filter '/coverage/'
 
- 
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Views', 'app/views'
 end
