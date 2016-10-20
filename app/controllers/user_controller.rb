81e0eb1bbf83c5adb984cedfc3bcf60112d140dc
Varun Mathuria <v.mathuria@berkeley.edu>
2016-10-20 17:58:20 +0000

    Added basic edit view and update method to controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,38 @@
 class UserController < ApplicationController
-  def hello
+  def new
+    @user = User.new
   end
+  
+  # def index
+  #   @users = User.all
+  # end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You signed up successfully!"
+    else
+      # flash[:notice] = "Form is invalid"
+      render "new"
+    end
+  end
+  
+  def edit
+    @user = User.find params[:id]
+  end
+  
+  def update
+    @user = User.find params[:id]
+    @user.update_attributes!(user_params)
+    @team = @user != nil ? @user.team : nil
+    return redirect_to team_path({:id => @team === nil ? 1 : @team.id, :uid => user_id})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
-  root "application#index"
+  root "user#new"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -63,4 +63,5 @@ Rails.application.routes.draw do
   #   end
   
   resources :team
+  resources :user
 end
