f6b9b3d36cdaf0b8ec8e4dc378ff0e4382a0cc0d
David <dkoh@berkeley.edu>
2016-10-21 06:46:30 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :team
   
-  """
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: VALID_EMAIL_REGEX
@@ -9,7 +8,7 @@ class User < ActiveRecord::Base
   validates :major, presence: true, length: { maximum: 50 }
   validates :sid, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :password, presence: true, length: {maximum: 50 }
-  """
+  
   #has_secure_password
   
     
