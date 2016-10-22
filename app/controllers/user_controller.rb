0a9ce5cc41385c0964263ce5b88e3ecf6f795c0c
Dasol Yoon <dasolyoon@berkeley.edu>
2016-10-22 23:46:48 +0000

    checks if user is logged in, validates the user and team they want to join

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,9 @@ class UserController < ApplicationController
   # before_filter :authenticate
   
   def new
+    #TODO: REMEMBER TO CHECK WEN SOMEONE CREATES A NEW USER THAT THE USER DOESNT ALREAY EXIST
     @user = User.new
+    session[:user_id] = @user.id
   end
   
   def create
@@ -23,21 +25,36 @@ class UserController < ApplicationController
   end
   
   def without
-  	render :layout => "layouts/without_team"
-  end  
+    if session[:user_id].nil?
+      redirect_to login_path
+    else
+    	render :layout => "layouts/without"
+    end
+  end
 
   def start_team
+    # TODO: function in team_controller to generate team hashes
     @user = User.find(session[:user_id])
-    @user.team = Team.create!(:passcode => User.name + "'s team hash", :approved => false)
+    @user.team = Team.create!(:passcode => @user.name + "'s team hash", :approved => false)
     redirect_to team_path(:id=>@user.team.id)
-  end  
+  end
+
   def join_team
-    @team_id = params[:team_hash]
-    @user = User.find(session[:user_id])
-    @team = Team.find(@team_id)
-    @user.team = @team
+    @team_passcode = params[:team_hash]
+    if !!(@team_passcode.empty?) or !(Team.exists?(:passcode => @team_passcode))
+      flash[:notice] = "Please enter a valid team passcode"
+      redirect_to without_team_path
+    else
+      # TODO: on team page, only show current user's team
+      # if user not logged in, redirect to login page
+      # if user not on a team, redirect to without_team page
+      flash[:notice] = "Successfully created a team!"
+      @user = User(session[:user_id])
+      @team = Team.find_by_passcode(@team_passcode)
+      @user.team = @team
+      redirect_to team_path(:id=>@team.id)
+    end
     
-    redirect_to team_path(:id=>params[:team_hash])
   end
 
   def update
