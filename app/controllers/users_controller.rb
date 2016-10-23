130c17fbd54a1f92ad4a196abc09fe14a218da82
Dasol Yoon <dasolyoon@berkeley.edu>
2016-10-23 20:38:01 +0000

    fixed minor issues from PR

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to without_team_path, :notice => "You signed up successfully!"
@@ -37,15 +37,14 @@ class UsersController < ApplicationController
     @team = Team.create!(:passcode => @user.name + "'s team hash", :approved => false)
     @user.team = @team
     @team.users << @user
-    flash[:notice] = "Successfully created a team!"
-    redirect_to team_path(:id=>@team.id)
+    redirect_to team_path(:id=>@team.id), :notice => "Successfully created a team!"
   end
 
   def join_team
+    # TODO: How to make all other views use application view for header?
     @team_passcode = params[:team_hash]
-    if !!(@team_passcode.empty?) or !(Team.exists?(:passcode => @team_passcode))
-      flash[:notice] = "Please enter a valid team passcode"
-      redirect_to without_team_path
+    if @team_passcode.empty? or !(Team.exists?(:passcode => @team_passcode))
+      redirect_to without_team_path, :notice => "Please enter a valid team passcode"
     else
       # TODO: when editing info, pre-fill in boxes
       # also when we integrate calnet, email/password box shouldn't be there
