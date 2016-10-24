47d9255db5aaaaa2a233d113e4a4ef1d043e54c5
David <dkoh@berkeley.edu>
2016-10-24 20:56:47 +0000

    added dropdown menu for majors in new and edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: VALID_EMAIL_REGEX
-  # validates :team, presence: true, length: { maximum: 50 }
-  #validates :major, presence: true, length: { maximum: 50 }
   validates :sid, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :password, presence: true, length: {maximum: 50 }
   
