0fdffed555e419ef59ec654765ffe7c0bb01520d
Adnan Hemani <adnan.h@berkeley.edu>
2016-10-24 07:59:51 +0000

    Trying to rescue heroku errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,6 @@ class ApplicationController < ActionController::Base
    if Rails.env.production?
     unless Rails.application.config.consider_all_requests_local
       rescue_from Exception, with: :render_500
-      rescue_from ActionController::RoutingError, with: :render_404
-      rescue_from ActionController::UnknownController, with: :render_404
-      rescue_from ActionController::UnknownAction, with: :render_404
-      rescue_from ActiveRecord::RecordNotFound, with: :render_404
     end
   end
   
