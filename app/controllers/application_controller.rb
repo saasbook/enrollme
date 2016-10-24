cfe201b373d405f2c7bd13bff4534a31777b167b
Adnan Hemani <adnan.h@berkeley.edu>
2016-10-24 07:57:01 +0000

    Trying to rescue heroku errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,32 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+   if Rails.env.production?
+    unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: :render_500
+      rescue_from ActionController::RoutingError, with: :render_404
+      rescue_from ActionController::UnknownController, with: :render_404
+      rescue_from ActionController::UnknownAction, with: :render_404
+      rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    end
+  end
+  
+  def render_404(exception)
+    @not_found_path = exception.message
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
+  def render_500(exception)
+    logger.info exception.backtrace.join("\n")
+    respond_to do |format|
+      format.html { render template: 'errors/internal_server_error', layout: 'layouts/application', status: 500 }
+      format.all { render nothing: true, status: 500}
+    end
+  end
+  
   def index
     @user_id = session[:user_id]
     @user = User.where(:id => @user_id).first
@@ -12,4 +38,6 @@ class ApplicationController < ActionController::Base
 
     return redirect_to team_path(:id => @user.team.id)
   end
+  
+  
 end
