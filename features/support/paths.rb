6050ec0df3a7e358bbb549f822438e940ed4129c
Dasol Yoon <dasolyoon@berkeley.edu>
2016-11-02 06:22:43 +0000

    Adding back deleted path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -21,6 +21,8 @@ module NavigationHelpers
       "/admins/" + $1
     when /^the team "([^"]*)" page$/i
       team_path(:id=>$1)
+    when /^the removal page for "([^"]*)"$/i
+      edit_team_path(:id => 0, :unwanted_user=>User.find_by_name($1))
     when /^the approve team "([^"]*)" page$/i
       approve_team_path(:team_id=>$1)
     when /^the disapprove team "([^"]*)" page$/i
