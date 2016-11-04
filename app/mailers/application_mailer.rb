a1cd8ce9f662376c2b547b12825961244f0f1135
dasolyoon <dasolyoon@berkeley.edu>
2016-11-03 23:47:56 -0700

    100% covered (#37)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,10 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   
-  def email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Submit Group Form')
-  end
-  
   layout 'mailer'
 end
