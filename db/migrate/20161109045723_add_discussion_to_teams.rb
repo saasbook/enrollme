1ea66bccb34c8dfc6c4d26e6cdda47d649e2b377
Dasol Yoon <dasolyoon@berkeley.edu>
2016-11-09 06:22:38 +0000

    trying

--- db/migrate/20161109045723_add_discussion_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddDiscussionToTeams < ActiveRecord::Migration
-  def change
-    add_reference :teams, :discussion, index: true, foreign_key: true
-  end
-end
--- db/migrate/20161109045812_add_team_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddTeamToUsers < ActiveRecord::Migration
-  def change
-    add_reference :users, :team, index: true, foreign_key: true
-  end
-end
