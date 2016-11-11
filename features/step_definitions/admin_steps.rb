71fb6f5fa9f30643a5e6216d8c5255e5272a1e62
Adnan Hemani <adnan.h@berkeley.edu>
2016-11-11 05:49:31 +0000

    fixed duplicates and moved pending all to one file called loggin_steps prepare for merge :

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -18,15 +18,3 @@ When /^I upload an invalid file$/ do
   attach_file(:discussions, File.join('features', 'test_files', 'bad_file.rb'))
   click_button "Upload"
 end
-
-Then(/^I should not see that team$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^PENDING: I check "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I should see "([^"]*)" or "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
--- features/step_definitions/loggin_steps.rb
+++ features/step_definitions/loggin_steps.rb
@@ -5,3 +5,61 @@ end
 Given(/^P I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
   pending # Write code here that turns the phrase above into concrete actions
 end
+Given(/^PENDING: the following admins exist$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^PENDING: the time is (\d+):(\d+)$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+
+Given(/^PENDING: the following users exist$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^PENDING: I am on the login page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^PENDING: I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^PENDING: I press "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^PENDING: I follow "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^PENDING: I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^PENDING: I should not see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+
+Then(/^I should not see that team$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)" or "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^P I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^PENDING: I check "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
