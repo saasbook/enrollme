1888d23fb780c7524c61e17afc5e973a4dcc0afe
Adnan Hemani <adnan.h@berkeley.edu>
2016-11-14 01:22:00 +0000

    Fixing Submitted and Creating new teams bugs

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,29 @@ ActiveRecord::Schema.define(version: 20161116015013) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "applications", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "disc1id"
+    t.integer  "disc2id"
+    t.integer  "disc3id"
+  end
+
   create_table "discussions", force: :cascade do |t|
     t.integer "number"
     t.string  "time"
     t.integer "capacity"
+<<<<<<< fd651ed04fe649dfa17a140a0fba44840e6ded02
     t.integer "submission_id"
   end
 
   add_index "discussions", ["submission_id"], name: "index_discussions_on_submission_id"
+=======
+    t.integer "application_id"
+  end
+
+  add_index "discussions", ["application_id"], name: "index_discussions_on_application_id"
+>>>>>>> Fixing Submitted and Creating new teams bugs
 
   create_table "submissions", force: :cascade do |t|
     t.integer  "disc1id"
@@ -49,14 +64,16 @@ ActiveRecord::Schema.define(version: 20161116015013) do
     t.integer  "discussion_id"
   end
 
+  add_index "teams", ["application_id"], name: "index_teams_on_application_id"
   add_index "teams", ["discussion_id"], name: "index_teams_on_discussion_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "email",      null: false
-    t.string   "password",   null: false
-    t.string   "major",      null: false
-    t.string   "sid",        null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "team"
+    t.string   "major"
+    t.string   "sid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "team_id"
