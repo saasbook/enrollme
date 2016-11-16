25c0ac1b9a37adcf0816f2db5dd9cdd61f08ae64
Dasol Yoon <dasolyoon@berkeley.edu>
2016-11-16 07:20:33 +0000

    changed a little routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
-  post 'logout', to: 'session#destroy'
+  get 'logout', to: 'session#destroy'
   
   resources :admins
   get 'approve_team', to: 'admins#approve'
