4d928a56e357b3fc3daa5b6b8f7bd385ff386473
David <dkoh@berkeley.edu>
2016-11-17 05:53:55 +0000

    changed route and removed unnecessary layout/discussion.html.erb

--- app/controllers/discussion_controller.rb
+++ app/controllers/discussion_controller.rb
@@ -2,15 +2,22 @@ class DiscussionController < ApplicationController
    
    def show
       @discussions = Discussion.all
-      
    end
    
    def submit
       puts params[:id]
       @discussions = Discussion.all
-      
-      
       # @discussions = Discussion.where("select IN (?)", params[:id])
+   end
+   
+   def add
+      puts "EECS"
+   end
+   
+   def complete
+      params[:task_id]
+      # mark checkboxes as complete
+      redirect_to upload_discussion_info_path
    end
    
 end
