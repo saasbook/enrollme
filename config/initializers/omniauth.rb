620a8bf8e185cfd54e71ecab64d7d6c9d9c59d87
Varun Mathuria <v.mathuria@berkeley.edu>
2016-11-21 19:31:05 +0000

    Revert "hold on I messed up my env"
    This reverts commit f1936cab41707379ddd19172f6ebbe5a4c3117b8.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '31784258058-vbo1utcvlanmov42ocu49m91tgdjb18s.apps.googleusercontent.com', 'ONtJ8NLrYKDwmDS06S9dlDTo', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
