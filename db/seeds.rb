781d260dfa57d97ada542b122d76ac49b584b1d1
Jason Gao <jlg@berkeley.edu>
2016-11-24 22:15:27 +0000

    Cleans up seeds by only keeping the necessary admins to initialize the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,81 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-people = [
-  		{ :name => "John"                   ,:major => "English"  , :sid => "111"  ,:email => "111@berkeley.edu"},
-  		{ :name =>  "Josh"                   ,:major => "CS"  ,:sid => "222"  ,:email => "222@berkeley.edu"},
-  		{ :name =>  "Jorge"                   ,:major => "English"  ,:sid => "333"  ,:email => "333@berkeley.edu"},
-  		{ :name =>  "CCC"                   ,:major => "EECS"  ,:sid => "444"  ,:email => "444@berkeley.edu"},
- 	  	{ :name =>  "DDD"                   ,:major => "English"  ,:sid => "555"  ,:email => "555@berkeley.edu"},
- 	  	{:name =>  "EEE"                   , :major =>"English"  ,:sid => "666"  ,:email => "666@berkeley.edu"},
- 	  	{:name => "FFF"                   ,:major => "EECS"  ,:sid => "777" ,:email => "777@berkeley.edu"},
- 	  	{:name => "GGG"                   ,:major => "CS"  ,:sid => "888" ,:email => "888@berkeley.edu"},
- 	  	{:name =>  "HHH"                   ,:major => "English"  ,:sid => "999" ,:email =>"999@berkeley.edu"},
- 	  	{:name =>  "III"                   ,:major => "English"  ,:sid => "1111" ,:email => "1111@berkeley.edu"},
- 	  	{:name =>  "JJJ"                   ,:major => "English"  ,:sid => "2222" ,:email => "2222@berkeley.edu"},
- 	  	{:name =>  "Kay"                   ,:major => "EECS"  ,:sid => "3333" ,:email => "3333@berkeley.edu"},
- 	  	{:name =>  "Ana"                   ,:major => "CS"  ,:sid => "4444" ,:email => "4444@berkeley.edu"}
-    ]
-    
-people.each do |ue|
-  User.create!( ue)
-end
-
-
-
-tone = Team.new
-tone.approved = false
-tone.submitted = false
-tone.passcode = "passcode1"
-tone.save!
-tone.users << User.where(name: "Josh")
-tone.users << User.where(name: "CCC")
-tone.users << User.where(name: "GGG")
-tone.users << User.where(name: "Jorge")
-tone.users << User.where(name: "EEE")
-
-
-
-
-two = Team.new
-two.approved = false
-two.submitted = false
-two.passcode = "passcode2"
-two.save!
-two.users << User.where(name: "JJJ")
-two.users << User.where(name: "DDD")
-two.users << User.where(name: "John")
-
-tthree = Team.new
-tthree.approved = false
-tthree.submitted = false
-tthree.passcode = "passcode3"
-tthree.save!
-tthree.users << User.where(name: "Kay")
-tthree.users << User.where(name: "Ana")
-tthree.users << User.where(name: "III")
-
-
 admins = [
-  		{ :name => "Anna", :email => "sid.masih@berkeley.edu"},
-  		{ :name => "Bruce", :email => "dkoh@berkeley.edu"},
-  		{ :name => "EnrollMe", :email => "enrollmeberkeley@gmail.com"},
-  		{ :name => "Rihanna", :email => "v.mathuria@berkeley.edu"}
+      { :name => "EnrollMe", :email => "enrollmeberkeley@gmail.com", :superadmin => false},
+  		{ :name => "Michael-David Sasson", :email => "sasson@berkeley.edu", :superadmin => true},
+  		{ :name => "Tony Lee", :email => "tony.lee@berkeley.edu", :superadmin => false}
   ]
   
 admins.each do |a|
   Admin.create!(a)
 end
-
-
-full = Discussion.create!(:number => 666, :day=> "Wednesday", :time=> "3pm", :capacity => 3)
-somewhat_full = Discussion.create!(:number => 420, :day=> "Thursday", :time=> "3pm", :capacity => 5)
-spank = Discussion.create!(:number => 504, :day=> "Friday", :time=> "2pm", :capacity => 20)
-Discussion.create!(:number => 1337, :day=>"Wednesday", :time=> "3pm", :capacity => 5)
-Discussion.create!(:number => 1338, :day=>"Thusday", :time=> "3pm", :capacity => 5)
-Discussion.create!(:number => 1339, :day=>"Friday", :time=> "3pm", :capacity => 5)
-
-
-full.teams << two
-somewhat_full.teams << tone
-spank.teams << tthree
