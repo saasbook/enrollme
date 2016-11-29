eb013d350f47d6da6cfa9db6b82ae86ea68b6d08
Dasol Yoon <dasolyoon@berkeley.edu>
2016-11-29 21:32:58 +0000

    changed secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
     port: 587,
     domain: 'gmail.com',
     user_name: 'enrollmeberkeley@gmail.com',
-    password: 'xsuexspmvwbyifht',
+    password: ENV['API_KEY'],
     authentication: 'plain',
     #enable_starttls_auto: true
   }
