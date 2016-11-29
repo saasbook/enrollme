24a9fee3081e6ca7e18a7bd8747b285b67d9c064
David <dkoh@berkeley.edu>
2016-11-29 06:59:32 +0000

    sending team approved / disapproved emails work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
     port: 587,
     domain: 'gmail.com',
     user_name: 'enrollmeberkeley@gmail.com',
-    password: 'eecseecseecs',
+    password: 'xsuexspmvwbyifht',
     authentication: 'plain',
     enable_starttls_auto: true
   }
