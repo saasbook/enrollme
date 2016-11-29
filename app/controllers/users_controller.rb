dd2bb8a05d181d31db654ef9c1be752650d0e8eb
Varun Mathuria <v.mathuria@berkeley.edu>
2016-11-29 20:56:45 +0000

    Uncommented welcome_email line, I had previously commented it out so I wouldn't hit that bug that adnan and dasol are working on

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
     if @user.save
       EmailStudents.welcome_email(@user).deliver_later
+
       session[:user_id] = @user.id
       # session[:user_email] = @user.email
       redirect_to without_team_path, :notice => "You signed up successfully!"
