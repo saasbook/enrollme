0de3b212e236aeaa48ab1c97c7684f304fe2510d
Varun Mathuria <v.mathuria@berkeley.edu>
2016-11-29 20:22:53 +0000

    User cannot sign up if he or she is an admin and admin cannot sign up another admin if he or she is a user. Also cleared session before creating a new one to fix a bug I've been seeing

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,8 @@ class Admin < ActiveRecord::Base
     
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: VALID_EMAIL_REGEX
+  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, \
+    format: VALID_EMAIL_REGEX, exclusion: { in: User.pluck(:email) }
   
   def self.admin_from_oauth(auth)
     return Admin.find_by(:email => auth[:info][:email])
