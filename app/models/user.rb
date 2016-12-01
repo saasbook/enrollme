1d19f9e3be66ad8d49c51203e66fc9b5000eafe7
Varun Mathuria <v.mathuria@berkeley.edu>
2016-12-01 22:58:34 +0000

    Fixed validation of user and admin, changed something in session that I'm not 100% sure of, review would be good

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, \
-    format: VALID_EMAIL_REGEX, exclusion: { in: Admin.pluck(:email) }
+    format: VALID_EMAIL_REGEX, exclusion: { in: lambda { |u| u.all_admin_emails } }
   validates :major, presence: true
   validates :sid, presence: true, uniqueness: true, length: { maximum: 10 }
   
@@ -21,6 +21,10 @@ class User < ActiveRecord::Base
   
   def self.user_from_oauth(auth)
     return User.find_by(:email => auth[:info][:email])
+  end
+  
+  def all_admin_emails
+    return Admin.pluck(:email)
   end
 
 end
