dff0364459c41eb7adc3edec928e0ac1836f2fbc
Varun Mathuria <v.mathuria@berkeley.edu>
2016-12-02 03:06:13 +0000

    What the heck why isn't this merge conflict thing getting deleted?!

--- features/step_definitions/enrollme_steps.rb
+++ features/step_definitions/enrollme_steps.rb
@@ -94,13 +94,11 @@ Then /^(?:|I )should not be on (.+)$/ do |page_name|
   end
 end
 
-<<<<<<< HEAD
+
 Then /^byebug$/ do
   byebug
 end
 
-=======
->>>>>>> 5ead40ba88792de9f3ffc1224271da9cc6eaf07b
 Then /^print page body$/ do
   puts page.body
 end
