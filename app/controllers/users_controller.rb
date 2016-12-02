e63ea8de2e08ed1ccf0d4c45652c7495423d3252
Dasol Yoon <dasolyoon@berkeley.edu>
2016-12-02 00:49:45 +0000

    tried to reduce cycl complexity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,8 @@ class UsersController < ApplicationController
   def join_team
     @passcode = params[:team_hash]
     @team = Team.find_by_passcode(@passcode)
-    return redirect_to without_team_path, :notice => "Unable to join team" if @passcode.empty? or @team.nil? or @team.approved or @team.users.size == 6
+    @team ||= Team.new()
+    return redirect_to without_team_path, :notice => "Unable to join team" if @passcode.empty? or !(@team.can_join?)
     
     @user.leave_team if !(@user.team.nil?)
     
