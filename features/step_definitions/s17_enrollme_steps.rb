4b3776fd337527c0336ddcee7a527ffa836f0a08
DerekHs <hsiaoderek@berkeley.edu>
2017-07-09 23:06:42 -0700

    finished george's regexes

--- features/step_definitions/s17_enrollme_steps.rb
+++ features/step_definitions/s17_enrollme_steps.rb
@@ -17,3 +17,23 @@ end
 Then /I? should see the file (.*)/ do |file|
   pending
 end
+
+And /the table should list the teams with (?:all)? (.*) first, then (?:the)? (.*)/ do |first, last|
+  pending
+end
+
+Given /I type "([^"]*)" into (?:the)? "([^"]*)"/ |text, field|
+  pending
+end
+
+Then /It should display the teams with (.*) as members/ do |members|
+  pending
+end
+
+Given /I press on row (.*)/ do |row_num|
+  pending
+end
+
+Then /I visit the team profile of the team on (.*)/ do |row_num|
+  pending
+end
