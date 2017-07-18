29c4d307e3622c624d59a1bbb6b56b4482bd6d05
George <georgesu1074@berkeley.edu>
2017-07-18 22:51:04 +0000

    Debugging declared tag. Will probably replace with waitlisted tag anyways though.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,33 +10,36 @@ class TeamsController < ApplicationController
     when 'default'
       ordering, @num_members_header = {:users_count => :desc}, 'hilite'
     when 'users_count'
-      if session[:ordering][:num_members] == :desc
-        ordering,@num_members_header = {:users_count => :asc}, 'hilite'
-      else
-        ordering,@num_members_header = {:users_count => :desc}, 'hilite'
+      if params[:sort]
+        if session[:ordering]["users_count"] == "desc"
+          ordering,@num_members_header = {:users_count => :asc}, 'hilite'
+        else
+          ordering,@num_members_header = {:users_count => :desc}, 'hilite'
+        end
       end
     when 'pending_requests'
-      if session[:ordering][:pending_requests] == :desc
+      if session[:ordering]["pending_requests"] == "desc"
         ordering,@pending_requests_header = {:pending_requests => :asc}, 'hilite'
       else
         ordering,@pending_requests_header = {:pending_requests => :desc}, 'hilite'
       end
     end
-    @majors = Team.all_declared
-    @selected_majors = params[:declared] || session[:declared] || {}
+    @all_majors = Team.all_declared
+    @selected_majors = params[:major] || session[:major] || {}
     
     if @selected_majors == {}
-      @selected_majors = Hash[@majors.map {|major| [major, major]}]
+      @selected_majors = Hash[@all_majors.map {|m| [m, m]}]
     end
     
-    if params[:sort] != session[:sort] or params[:declared] != session[:declared]
+    if params[:sort] != session[:sort] or params[:major] != session[:major]
       session[:sort] = sort
-      session[:declared] = @selected_majors
-      redirect_to :sort => sort, :declared => @selected_majors and return
+      session[:major] = @selected_majors
+      redirect_to :sort => sort, :major => @selected_majors and return
     end
     
     session[:ordering] = ordering
-    # @teams = Team.where(declared: @selected_majors.keys).order(ordering)
+    flash[:notice] = "all_majors is #{@all_majors}, and selected_majors is #{@selected_majors}, and selected_majors.keys is #{@selected_majors.keys}"
+    #@teams = Team.where(declared: @selected_majors).order(ordering) # this line is bugged?
     @teams = Team.order(ordering)
   end
   
