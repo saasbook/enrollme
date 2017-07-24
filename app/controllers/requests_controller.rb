bad9a0957f866efde4d12223e82a09d504ef4c15
Hadi Zhang <hadizhang@berkeley.edu>
2017-07-24 01:00:52 +0000

    (quick) Added ability to create a request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,7 +4,8 @@ class RequestsController < ApplicationController
     end
 
     def create
-        request = Request.new(request_params)
+        Request.create!(request_params)
+        redirect_to teams_path
     end
     
     def index
