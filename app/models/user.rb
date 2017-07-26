5a885a9e08163e92430ab7a17d7ad6d5bfcc86a2
Ken Chiu <kenchiu100@gmail.com>
2017-07-26 22:05:10 +0000

    AddAttachmentAvatarToUsers.Modify model and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   belongs_to :team, counter_cache: true
   has_many :requests
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   #has_many :teams, through: :requests
   
   validates :name, presence: true, length: { maximum: 50 }
