8b74530a20a6194b3c9d546dc99a745887365f55
Ken Chiu <kenchiu100@gmail.com>
2017-08-05 03:58:08 +0000

    finish current state highlight for students perspective

--- app/assets/javascripts/_head.js
+++ app/assets/javascripts/_head.js
@@ -1,4 +1,12 @@
-$(".nav a").on("click", function(){
-  $(".nav").find(".active").removeClass("active");
-  $(this).parent().addClass("active");
-});
+// $(".nav a").on("click", function(){
+//   $(".nav li").find(".active").removeClass("active");
+//   $(this).parent().addClass("active");
+
+// });
+
+// var selector = '.nav li';
+
+// $(selector).on('click', function(){
+//     $(selector).removeClass('active');
+//     $(this).addClass('active');
+// });
