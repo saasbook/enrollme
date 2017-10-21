7c117dcd8160e703050a43ff9c2e1a0be955be6c
Raymond Chan <raychan3@users.noreply.github.com>
2017-10-20 23:32:14 -0700

    Broken fixes on filling sign up email form (#13)
    * Broken fixes on filling sign up email form
    
    * fixed user form logic
    
    * fixed new feature bug
    
    * Edit admin_mailer style
    
    * Comment out unused email functionality
    
    Signed-off-by: Colin Schoen <cschoen2020@gmail.com>
    
    * Some style changes
    
    * More fixes
    
    * Remove cucumber_output
    
    * indentation fixes for admin_mailer
    
    * Lines max width

--- features/support/env.rb
+++ features/support/env.rb
@@ -79,7 +79,6 @@ World(FactoryGirl::Syntax::Methods)
 OmniAuth.config.test_mode = true
 
 module OmniauthMacros
-  def mock_auth_hash(email)
     # The mock_auth configuration allows you to set per-provider (or default)
     # authentication hashes to return during integration testing.
     OmniAuth.config.mock_auth[:google] = {
@@ -94,10 +93,9 @@ module OmniauthMacros
     # 'secret' => 'mock_secret'
     # }
       :info => {
-        :email => email
+        :email => 'eecs666@hotmail.com'
       }
     }
-  end
 end
 
 World(OmniauthMacros)
