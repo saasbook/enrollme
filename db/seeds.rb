241f0b00a464d77f5e5ae980b87d6d54416425e1
Raymond Chan <raymondchan243@gmail.com>
2017-11-04 01:05:53 +0000

    [fix] Revert seeds to only have admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def createObjs(model, objs)
-  model.delete_all
-  objs.each {|obj| model.create! obj}
-end
-
 admins = [
       { :name => "EnrollMe", :email => "enrollmeberkeley@gmail.com", :superadmin => true},
       { :name => "Michael-David Sasson", :email => "sasson@berkeley.edu", :superadmin => false},
       { :name => "Test Admin", :email => "enrollme.test@gmail.com", :superadmin => false}
   ]
 
-createObjs(Admin, admins)
+Admin.delete_all
+admins.each do |a|		
+  Admin.create!(a)		
+end
 
 Option.delete_all
 Option.create!(
   :minimum_team_size => 3,
   :maximum_team_size => 3
   )
-
-# users = [
-#   {:name => "Bob Smith", :email => "bob@smith.com", :sid => "12345678", :major => "CS"}
-# ]
-# createObjs(User, users)
-
-# teams = [
-#   {:approved => true, :passcode => "somePasscode"}
-# ]
-
-# createObjs(Team, teams)
-
-# Team.all.each do |t|
-#   user = User.where(:email => "bob@smith.com")[0]
-#   t.users.append(user)
-#   t.save!
-#   user.team = t
-#   user.save!
-# end
