9749851fcaa80060b733d9611ce7430d3d1e4c6f
Colin Schoen <cschoen2020@gmail.com>
2017-11-17 19:03:28 +0000

    Fix style issues

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -3,8 +3,8 @@ class TeamMailer < ApplicationMailer
 
   def email_team(to, subject, body, reply_to)
     @body = body
-    @url = ENV["SERVER_EMAIL"]
-    mail(to: to, reply_to: reply_to, subject: subject) do |format| 
+    @url = ENV['SERVER_EMAIL']
+    mail(to: to, reply_to: reply_to, subject: subject) do |format|
       format.html
     end
   end
