cdbd3a42ca444618b799c0be9f3e7cd23ad7a64f
yonas024 <kbromyonas@berkeley.edu>
2017-11-19 22:27:50 +0000

    heroku email test

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'enrollmeberkeley@gmail.com',
+  :password             => ENV['API_KEY'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  
+}
